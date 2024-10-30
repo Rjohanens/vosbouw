84514d0350370fdd04de8aa497d533dbf628c813
rjohannes <rick@appademic.nl>
2024-10-30 22:42:06 +0100

    removed mail

--- app/Livewire/Website/Pages/Home.php
+++ app/Livewire/Website/Pages/Home.php
@@ -43,8 +43,8 @@ class Home extends Component
                 )
             );
 
-        Mail::to($this->form->email)
-            ->send(new ContactConfirmationMail());
+        // Mail::to($this->form->email)
+        //     ->send(new ContactConfirmationMail());
 
         session()->flash('success-message', 'Uw bericht is succesvol verzonden!');
         $this->form->reset();
