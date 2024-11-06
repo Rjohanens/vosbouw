dda6efe3d8a4a820f24bf3e99b88422f14c573c0
rjohannes <rick@appademic.nl>
2024-11-06 22:06:00 +0100

    fixed overflow projects

--- resources/views/livewire/website/pages/projects.blade.php
+++ resources/views/livewire/website/pages/projects.blade.php
@@ -1,7 +1,7 @@
-<div class="px-8 lg:px-32 py-24 lg:py-32">
+<div class="px-8 lg:px-32 py-24 lg:py-32 overflow-x-hidden">
     <h1 class="text-4xl text-zinc-700">Projecten</h1>
     <div class="w-12 h-1 bg-primary rounded-full mt-4"></div> 
-    <div class="flex items-center w-full space-x-4 mt-16">
+    <div class="flex flex-wrap justify-start items-center w-full gap-4 mt-16">
         <span class="text-sm text-zinc-500 text-nowrap">Filter op:</span>
         <div wire:click="clearFilter" class="px-2 text-nowrap rounded-full hover:bg-orange-500 hover:text-white transition duration-300 cursor-pointer @if($filter === '') bg-primary text-white @else bg-zinc-200 text-zinc-700 @endif">
             <span class="text-xs">Alle projecten</span>
