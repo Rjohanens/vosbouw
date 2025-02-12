23938c9e1ca70e496393ee6f821e4cdaba5ea3a1
rjohannes <rick@appademic.nl>
2025-02-12 22:28:16 +0100

    add favicon

--- resources/views/components/layouts/auth.blade.php
+++ resources/views/components/layouts/auth.blade.php
@@ -3,7 +3,12 @@
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>{{ $title ?? 'Vos Bouw B.V.' }}</title>
+
+	 <!-- icon -->
+	 <link rel="icon" href="{{ asset('images/logo.png') }}" type="image/png">
+	 
+	<title>{{ $title ?? 'Vos Bouw - Bouw- en aannemersberdrijf' }}</title>
+
 	@vite('resources/css/app.css')
 </head>
 <body class="font-rubik">
