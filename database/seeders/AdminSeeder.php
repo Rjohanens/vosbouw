a9264efe51789459cfc6324810469ec652fbc7f5
rjohannes <rick@appademic.nl>
2025-02-12 20:42:50 +0100

    added admin seeder

---
+++ database/seeders/AdminSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::factory()->create([
+            'name' => 'Robert Vos',
+            'email' => 'admin@vosbouwnoord.nl',
+            'email_verified_at' => now(),
+            'password' => Hash::make('V0tgizokP8zwtPY'),
+        ]);
+    }
+}
