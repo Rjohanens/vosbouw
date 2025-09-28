710fff581014fc484bf3a42993b6b37d816e67e5
rjohannes <rick@appademic.nl>
2025-09-28 21:29:25 +0200

    changed mailing list

--- app/Livewire/Website/Pages/Contact.php
+++ app/Livewire/Website/Pages/Contact.php
@@ -36,28 +36,6 @@ class Contact extends Component
                 )
             );
 
-          Mail::to('r.joty123@gmail.com')
-            ->send(
-                new ContactFormMail(
-                    $validated['firstName'],
-                    $validated['lastName'],
-                    $validated['email'],
-                    $validated['phone'],
-                    $validated['message'],
-                )
-            );
-
-        Mail::to('rickjohannes@hotmail.nl')
-            ->send(
-                new ContactFormMail(
-                    $validated['firstName'],
-                    $validated['lastName'],
-                    $validated['email'],
-                    $validated['phone'],
-                    $validated['message'],
-                )
-            );
-
         ContactRequest::create([
             'first_name' => $validated['firstName'],
             'last_name' => $validated['lastName'],
