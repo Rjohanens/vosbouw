ef25702d5f5e43c620144326b69cb4ef292df56c
rjohannes <rick@appademic.nl>
2025-09-28 19:18:45 +0200

    set mail header

--- app/Mail/ContactFormMail.php
+++ app/Mail/ContactFormMail.php
@@ -4,6 +4,7 @@ namespace App\Mail;
 
 use Illuminate\Bus\Queueable;
 use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Address;
 use Illuminate\Mail\Mailables\Content;
 use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
@@ -32,6 +33,7 @@ class ContactFormMail extends Mailable
     {
         return new Envelope(
             subject: 'Nieuwe contactaanvraag',
+            from: new Address('info@vosbouwnoord.nl', 'Vos Bouw'),
         );
     }
 
